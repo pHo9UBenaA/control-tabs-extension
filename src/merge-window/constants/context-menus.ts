a526ca8d62b3a61ec4c115746991088506ce8355
r-hashimoto <102408484+pHo9UBenaA@users.noreply.github.com>
2023-07-01 22:58:55 +0900

    refactor

--- src/merge-window/constants/context-menus.ts
+++ src/merge-window/constants/context-menus.ts
@@ -1,7 +1,7 @@
 const contextMenusIdsKey = ['mergeWindow', 'mergeSecretWindow'] as const;
 
-// TODO
 const contextMenusIds = {
+	// TODO
 	[contextMenusIdsKey[0]]: contextMenusIdsKey[0],
 	[contextMenusIdsKey[1]]: contextMenusIdsKey[1],
 } as const satisfies { [key in (typeof contextMenusIdsKey)[number]]: key };
