ccf81ef374862439ff41883debe08063c2e4316a
komiya <102408484+pHo9UBenaA@users.noreply.github.com>
2023-07-29 11:23:05 +0900

    Update Tab Cleaner (#46)

--- src/tab-cleaner/models/storage.ts
+++ src/tab-cleaner/models/storage.ts
@@ -1,48 +1,50 @@
 /**
  * ドメイン
  * v0.1.1 から v0.2.0 にアップデートする際、ドメインの型が変更されました。
- * 
+ *
  * @type {Domain}
  * @example 'google.com'
  * @example { uuid: 'c4b3d1c0-1f3b-11ec-9621-0242ac130002', name: 'google.com' }
  */
-type Domain = string | {
-	/**
-	 * uuid
-	 * 
-	 * @type {string}
-	 * @memberof Domain
-	 * @example 'c4b3d1c0-1f3b-11ec-9621-0242ac130002'
-	 */
-	uuid: string;
-	/**
-	 * ドメイン名
-	 * 
-	 * @type {string}
-	 * @memberof Domain
-	 * @example 'google.com'
-	 */
-	name: string;
-	/**
-	 * 履歴を保持するかどうか
-	 * 
-	 * @type {boolean | undefined}
-	 * @memberof Domain
-	 * @example true
-	 */
-	// isKeepHistory?: boolean;
-};
+type Domain =
+	| string
+	| {
+			/**
+			 * uuid
+			 *
+			 * @type {string}
+			 * @memberof Domain
+			 * @example 'c4b3d1c0-1f3b-11ec-9621-0242ac130002'
+			 */
+			uuid: string;
+			/**
+			 * ドメイン名
+			 *
+			 * @type {string}
+			 * @memberof Domain
+			 * @example 'google.com'
+			 */
+			name: string;
+			/**
+			 * 履歴を保持するかどうか
+			 *
+			 * @type {boolean | undefined}
+			 * @memberof Domain
+			 * @example true
+			 */
+			// isKeepHistory?: boolean;
+	  };
 
 /**
  * クリア履歴
- * 
+ *
  * @type {ClearHistory}
  * @example { id: 1, title: 'Google', url: 'https://www.google.com/' }
  */
 type ClearHistory = {
 	/**
 	 * タブID
-	 * 
+	 *
 	 * @type {number}
 	 * @memberof ClearHistory
 	 * @example 1
@@ -51,7 +53,7 @@ type ClearHistory = {
 	// TODO: (x): x is ClearHistory => x !== undefined
 	/**
 	 * タイトル
-	 * 
+	 *
 	 * @type {string}
 	 * @memberof ClearHistory
 	 * @example 'Google'
@@ -59,7 +61,7 @@ type ClearHistory = {
 	title: string | undefined;
 	/**
 	 * URL
-	 * 
+	 *
 	 * @type {string}
 	 * @memberof ClearHistory
 	 * @example 'https://www.google.com/'
@@ -68,12 +70,23 @@ type ClearHistory = {
 };
 
 /**
- * 新規タブを削除するかどうか
- * 
- * @type {boolean}
- * @example true
- * 
+ * 設定項目
  */
-type RemoveNewTab = boolean;
+type Setting = {
+	/**
+	 * 新規タブを削除するかどうか
+	 *
+	 * @type {boolean}
+	 * @memberof Setting
+	 */
+	removeNewTab: boolean;
+	/**
+	 * クリア履歴の削除上限
+	 *
+	 * @type {number}
+	 * @memberof Setting
+	 */
+	clearHistoriesLimit?: number;
+};
 
-export { ClearHistory, Domain, RemoveNewTab };
+export { ClearHistory, Domain, Setting };
